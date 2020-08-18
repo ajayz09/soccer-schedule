ce05e83904f4a31384abe4f7111bb728ec2bac14
ajayz09 <1994ajaymohan@gmail.com>
2020-08-18 12:13:21 +1000

    added search

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <Search></Search> -->
+    <DropDown/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Search from "./components/Search.vue";
+import DropDown from "./components/DropDown.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    DropDown
+    // Search
   }
-}
+};
 </script>
 
 <style>
@@ -22,7 +25,24 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+html {
+  overflow: hidden;
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  overflow: auto;
+  margin: 0;
+  background-color: #2c3e50;
+  /* background-image: url(assets/background.png);
+  background-size: contain; */
 }
 </style>
