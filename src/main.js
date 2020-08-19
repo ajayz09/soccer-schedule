50a994de19df4769a350679dff0e3fb90d1a38d1
ajayz09 <1994ajaymohan@gmail.com>
2020-08-19 12:25:11 +1000

    added vue material

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.css";
 
+import "vue-material/dist/theme/default.css";
+
 Vue.use(VueMaterial);
 Vue.config.productionTip = false;
 
