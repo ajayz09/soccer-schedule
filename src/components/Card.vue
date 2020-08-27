f50a3d6ccca4f4a90f149dab7daeb359bdd2cb83
ajayz09 <1994ajaymohan@gmail.com>
2020-08-27 16:10:28 +1000

    Update card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,46 +1,66 @@
 <template>
   <div class="card" @mouseover="hover = true" @mouseleave="hover = false">
-    <h3 style="padding:10px">FA Community Shield</h3>
-    <div class="row card-content">
-      <div class="col-sm-7 event">
-        <div class="row team-details">
-          <img
-            src="https://media.api-sports.io/football/teams/42.png"
-            alt="logo"
-            style="height:20px;width:20px;"
-          >
-          <h4>Liverpool</h4>
+    <h3 style="padding:10px">{{ item.league.name }}</h3>
+    <div class="row card-content" style="margin: auto">
+      <div class="col-sm-8 event">
+        <div class="row team-details" style="margin: auto">
+          <img v-bind:src="item.homeTeam.logo" alt="logo" style="height:20px;width:20px;">
+          <h4>{{ item.homeTeam.team_name }}</h4>
         </div>
-        <div class="row team-details">
-          <img
-            src="https://media.api-sports.io/football/teams/40.png"
-            alt="logo"
-            style="height:20px;width:20px;"
-          >
-          <h4>Arsenal</h4>
+        <div class="row team-details" style="margin: auto">
+          <img v-bind:src="item.awayTeam.logo" alt="logo" style="height:20px;width:20px;">
+          <h4>{{ item.awayTeam.team_name }}</h4>
         </div>
       </div>
-      <div class="col-sm-5 event-time">
-        <h5>30/08</h5>
-        <h5>Sunday</h5>
+      <div class="col-sm-4 event-time">
+        <h5>{{ formattedDate }}</h5>
+        <h5>{{ formattedTime }}</h5>
       </div>
     </div>
-    <p>location</p>
+    <p>{{ item.venue }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  data: () => ({})
+  data: () => ({
+    formattedTime: "",
+    formattedDate: ""
+  }),
+  props: {
+    item: {}
+  },
+  created() {
+    this.getTimeFromUNIX();
+  },
+  methods: {
+    getTimeFromUNIX() {
+      let unix_timestamp = this.item.event_timestamp;
+      var date = new Date(unix_timestamp * 1000);
+      var hours = date.getHours();
+      var minutes = "0" + date.getMinutes();
+      var when = "";
+      if (hours > 11) when = " pm";
+      else when = " am";
+      // Will display time in 10:30:23 format
+      this.formattedTime = hours + ":" + minutes.substr(-2) + when;
+      console.log(this.formattedTime);
+
+      var month = date.getMonth();
+      var day = date.getDate();
+      this.formattedDate = day + "/" + month;
+    }
+  }
 };
 </script>
 
 <style scoped>
 .card {
+  /* overflow-wrap: break-word; */
   position: relative;
-  height: 200px;
+  height: 180px;
   background: white;
-  width: 250px;
+  width: 300px;
   box-shadow: 0 5px 20px rgba(120, 120, 120, 0.4);
   border: none;
   border-radius: 20px;
@@ -64,14 +84,22 @@ export default {
 
 .team-details {
   padding: 5px;
+  justify-content: center;
+  align-self: center;
 }
 
 .team-details img {
-  margin: 0 5px 0px 30px;
+  margin-right: 5px;
 }
 
 .event-time {
   justify-content: center;
   align-self: center;
+}
+
+@media only screen and (max-width: 600px) {
+  .card {
+    height: 230px;
+  }
 }
 </style>
