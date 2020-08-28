95edf807db5e8e02700fb4085a7a5d32b4a25fbf
ajayz09 <1994ajaymohan@gmail.com>
2020-08-28 14:03:27 +1000

    epic spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,17 @@
+<template>
+  <fingerprint-spinner :animation-duration="1500" :size="64" color="#ff1d5e"/>
+</template>
+
+<script>
+import { FingerprintSpinner } from "epic-spinners";
+
+export default {
+  name: "Spinner",
+  components: {
+    FingerprintSpinner
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
