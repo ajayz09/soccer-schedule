785ead5214c2b4671960d3e2e222b49c3b48fb4b
ajayz09 <1994ajaymohan@gmail.com>
2020-08-28 14:51:51 +1000

    transistion changes

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,154 +1,109 @@
 <template>
-  <!-- <h1 style="color:white;">Inside Results</h1> -->
   <v-container fluid>
-    <div class="search-another">
-      <DropDown style="z-index:2"/>
-      <div class="search-another-btn">
-        <md-button id="search-btn" class="md-raised md-accent search-btn">Search</md-button>
-      </div>
+    <div v-if="loading" class="loading-spinner">
+      <Spinner/>
     </div>
-
-    <transition-group
-      name="slide-fade"
-      mode="out-in"
-      tag="div"
-      class="card-container"
-      style="z-index:1;"
-    >
-      <div class="event-container" v-for="(item, i) in items" :key="i">
-        <!-- <v-card id="schedule-card" class="ma-3 pa-6" :color="item.color" height="200">
-          <div class="d-flex flex-no-wrap justify-space-between">
-            <div>
-              <v-card-title class="headline" v-text="item.title"></v-card-title>
-
-              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
-            </div>
-
-            <v-avatar class="ma-3" size="125" tile>
-              <v-img
-                src="https://media.api-sports.io/football/leagues/528.png"
-                contain
-                max-width="70"
-                max-height="70"
-              >Sunday, 6:30pm</v-img>
-            </v-avatar>
-          </div>
-        </v-card>-->
-        <Card/>
+    <div v-else>
+      <div class="search-another">
+        <DropDown style="z-index:2" @onItemSelected="itemSelected" @onItemReset="itemReset"/>
+        <div class="search-another-btn">
+          <md-button
+            id="search-btn"
+            class="md-raised md-accent search-btn"
+            @click="searchAnotherSchedule()"
+          >Search</md-button>
+        </div>
+        <v-snackbar v-model="snackbar">
+          {{ text }}
+          <template v-slot:action="{ attrs }">
+            <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">Close</v-btn>
+          </template>
+        </v-snackbar>
       </div>
-    </transition-group>
 
-    <!-- <v-layout row wrap justify-space-between>
-      <v-flex xs12 md4 v-for="(item, i) in items" :key="i">
-        <v-card id="schedule-card" class="ma-3 pa-6" :color="item.color" height="200">
-          <div class="d-flex flex-no-wrap justify-space-between">
-            <div>
-              <v-card-title class="headline" v-text="item.title"></v-card-title>
-
-              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
-            </div>
-
-            <v-avatar class="ma-3" size="125" tile>
-              <v-img
-                src="https://media.api-sports.io/football/leagues/528.png"
-                contain
-                max-width="70"
-                max-height="70"
-              >Sunday, 6:30pm</v-img>
-            </v-avatar>
-          </div>
-        </v-card>
-      </v-flex>
-    </v-layout>-->
-
-    <!-- <v-row>
-      <v-col cols="12">
-        <v-row :align="center" :justify="space-between" centered>
-          <v-card
-            id="schedule-card"
-            class="ma-3 pa-6"
-            max-width="335"
-            v-for="(item, i) in items"
-            :key="i"
-            :color="item.color"
-          >
-            <div class="d-flex flex-no-wrap justify-space-between">
-              <div>
-                <v-card-title class="headline" v-text="item.title"></v-card-title>
-
-                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
-              </div>
-
-              <v-avatar class="ma-3" size="125" tile>
-                <v-img :src="item.src"></v-img>
-              </v-avatar>
-            </div>
-          </v-card>
-        </v-row>
-      </v-col>
-    </v-row>-->
-
-    <!-- <h1 style="color:white;">{{$route.query.q}}</h1> -->
-    <!-- <v-row>
-      <v-col cols="12">
-        <v-row :align="alignment" :justify="justify" class="grey lighten-5" style="height: 300px;">
-          <v-card v-for="n in 12" :key="n" class="ma-3 pa-6" outlined tile>Column</v-card>
-        </v-row>
-      </v-col>
-    </v-row>-->
+      <transition-group appear name="slide-in" tag="div" class="card-container" style="z-index:1;">
+        <div class="event-container" v-for="(item,i) in results" :key="i">
+          <Card :item="item"/>
+        </div>
+      </transition-group>
+    </div>
   </v-container>
 </template>
 
 <script>
 import DropDown from "./DropDown.vue";
 import Card from "./Card.vue";
+import Spinner from "./Spinner.vue";
+import json from "../../data/api.json";
 
 export default {
   name: "Results",
   created() {
-    console.log(this.$route);
-    // fetch(
-    //   "https://api-football-v1.p.rapidapi.com/v2/fixtures/team/" +
-    //     this.$route.query.q +
-    //     "/next/10",
-    //   {
-    //     method: "GET",
-    //     headers: {
-    //       "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-    //       "x-rapidapi-key": "c878d920a7msh960c77c641d7753p18ffd2jsn4467624a1a46"
-    //     }
-    //   }
-    // )
-    //   .then(response => {
-    //     console.log(response.json());
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   });
+    this.fetchResults();
   },
   components: {
     DropDown,
-    Card
+    Card,
+    Spinner
   },
   data: () => ({
-    items: [
-      {
-        color: "#1F7087",
-        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
-        title: "Supermodel",
-        artist: "Foster the People"
-      },
-      {
-        color: "#952175",
-        src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
-        title: "Halcyon Days",
-        artist: "Ellie Goulding"
+    dense: false,
+    snackbar: false,
+    text: "Select a team first",
+    isSelected: false,
+    selectedClub: [],
+    results: [],
+    loading: true,
+    apiData: json
+  }),
+  methods: {
+    fetchResults() {
+      console.log(this.$route);
+      fetch(
+        "https://api-football-v1.p.rapidapi.com/v2/fixtures/team/" +
+          this.$route.query.q +
+          "/next/12",
+        {
+          method: "GET",
+          headers: {
+            "x-rapidapi-host": this.apiData.host,
+            "x-rapidapi-key": this.apiData.key
+          }
+        }
+      )
+        .then(response => {
+          this.loading = false;
+
+          return response.json();
+        })
+        .then(data => {
+          this.results = data.api.fixtures;
+          console.log(this.results);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    itemSelected(item) {
+      this.isSelected = true;
+      this.selectedClub = item;
+    },
+    itemReset(value) {
+      this.isSelected = value;
+    },
+    searchAnotherSchedule() {
+      console.log("is selected", this.isSelected);
+      if (!this.isSelected) {
+        this.snackbar = true;
+        return;
       }
-    ],
-    // alignmentsAvailable: ["start", "center", "end", "baseline", "stretch"],
-    // alignment: "center",
-    dense: false
-  })
+      // Avoiding duplicate navigation
+      if (this.selectedClub.team_id != this.$route.query.q) {
+        this.$router.push("results?q=" + this.selectedClub.team_id);
+        this.fetchResults();
+      }
+    }
+  }
 };
 </script>
 
@@ -169,20 +124,32 @@ export default {
   border-radius: 8px !important;
 }
 
+.loading-spinner {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: -10;
+  /* background-color: white; */
+  background-color: rgba(236, 232, 232, 0.815);
+}
+
 #search-btn {
   height: 43px;
   margin-inline-end: auto;
 }
 
-.slide-fade-enter-active {
-  transition: all 0.3s ease;
+.fade-in-enter-active {
+  transition: all 3s ease;
 }
-.slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+
+.fade-in-leave-active {
+  transition: all 3s ease;
 }
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
-  transform: translateX(10px);
+
+.fade-in-enter,
+.fade-in-leave-to {
+  position: absolute; /* add for smooth transition between elements */
   opacity: 0;
 }
 </style>
